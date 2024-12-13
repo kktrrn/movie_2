7140b7463fb8f22f4cc56be0d76926df31e64b8e
darynazin <danayu@ukr.net>
2024-12-13 11:01:32 +0100

    Popular movies async fixed

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -598,39 +598,41 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"gLLPy":[function(require,module,exports,__globalThis) {
 var _networkJs = require("./modules/network.js");
 var _uiJs = require("./modules/ui.js");
-const popularMovies = await (0, _networkJs.getMovies)();
-const searchForm = document.getElementById("searchForm");
-const searchInput = document.getElementById("searchInput");
-popularMovies.forEach((movie)=>{
-    (0, _uiJs.renderMovie)(movie);
-});
-searchForm.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    const query = searchInput.value.trim().toLowerCase();
-    const section = document.getElementById("cards");
-    const message = document.getElementById("cardsHeader");
-    section.innerHTML = "";
-    if (query === "") {
-        message.textContent = "Popular movies";
-        message.style.display = "block";
-        popularMovies.forEach((movie)=>{
-            (0, _uiJs.renderMovie)(movie);
-        });
-    } else {
-        const filteredMovies = popularMovies.filter((movie)=>movie.title && movie.title.toLowerCase().includes(query) || movie.overview && movie.overview.toLowerCase().includes(query));
-        if (filteredMovies.length === 0) {
-            message.textContent = "No matches found";
+(async function initializeApp() {
+    const popularMovies = await (0, _networkJs.getMovies)();
+    const searchForm = document.getElementById("searchForm");
+    const searchInput = document.getElementById("searchInput");
+    popularMovies.forEach((movie)=>{
+        (0, _uiJs.renderMovie)(movie);
+    });
+    searchForm.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        const query = searchInput.value.trim().toLowerCase();
+        const section = document.getElementById("cards");
+        const message = document.getElementById("cardsHeader");
+        section.innerHTML = "";
+        if (query === "") {
+            message.textContent = "Popular movies";
             message.style.display = "block";
-        } else {
-            message.textContent = "Search results";
-            message.style.display = "block";
-            filteredMovies.forEach((movie)=>{
+            popularMovies.forEach((movie)=>{
                 (0, _uiJs.renderMovie)(movie);
             });
+        } else {
+            const filteredMovies = popularMovies.filter((movie)=>movie.title && movie.title.toLowerCase().includes(query) || movie.overview && movie.overview.toLowerCase().includes(query));
+            if (filteredMovies.length === 0) {
+                message.textContent = "No matches found";
+                message.style.display = "block";
+            } else {
+                message.textContent = "Search results";
+                message.style.display = "block";
+                filteredMovies.forEach((movie)=>{
+                    (0, _uiJs.renderMovie)(movie);
+                });
+            }
         }
-    }
-    searchInput.value = "";
-});
+        searchInput.value = "";
+    });
+})();
 
 },{"./modules/network.js":"2ZPcJ","./modules/ui.js":"lFOPJ"}],"2ZPcJ":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
